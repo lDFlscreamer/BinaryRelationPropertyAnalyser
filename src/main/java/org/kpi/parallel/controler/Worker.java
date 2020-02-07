85eff652665fc90fd141285ab219e2915c513189
lDFlscreamer <999screamer999@gmail.com>
2020-02-07 20:05:02 +0200

    fix analyse reflexivity

--- src/main/java/org/kpi/parallel/controler/Worker.java
+++ src/main/java/org/kpi/parallel/controler/Worker.java
@@ -12,31 +12,33 @@ public class Worker {
         Result res = new Result();
         ArrayList<RelationObj> Pr = new ArrayList<>();
         ArrayList<RelationObj> Ir = new ArrayList<>();
-        HashMap<Integer, ArrayList<Integer>> slice=new HashMap<>();
-        HashMap<Integer, ArrayList<Integer>> negativeSlice=new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> slice = new HashMap<>();
+        HashMap<Integer, ArrayList<Integer>> negativeSlice = new HashMap<>();
         boolean refl = true;
-        boolean antirefl = false;
+        boolean antirefl = true;
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    antirefl &= matrix[i][j] == 0;
+                    refl &= matrix[i][j] == 1;
+                }
                 if (matrix[i][j] == 1) {
-                    refl &= (i == j);//if stay true is reflexivity
                     if (matrix[j][i] == 1) {
-                        Ir.add(new RelationObj(i+1, j+1));
-                        Ir.add(new RelationObj(j+1, i+1));
+                        Ir.add(new RelationObj(i + 1, j + 1));
+                        Ir.add(new RelationObj(j + 1, i + 1));
                     } else {
-                        Pr.add(new RelationObj(i+1, j+1));
+                        Pr.add(new RelationObj(i + 1, j + 1));
                     }
                     ArrayList<Integer> currentSlice;
-                    currentSlice = slice.getOrDefault(i+1,new ArrayList<Integer>());
-                    currentSlice.add(j+1);
-                    slice.put(i+1,currentSlice);
+                    currentSlice = slice.getOrDefault(i + 1, new ArrayList<>());
+                    currentSlice.add(j + 1);
+                    slice.put(i + 1, currentSlice);
                 } else {
-                    antirefl |= (i == j);//if anti stay true is antireflixivity
                     ArrayList<Integer> currentSlice;
-                    currentSlice = negativeSlice.getOrDefault(i+1, new ArrayList<>());
-                    currentSlice.add(j+1);
-                    negativeSlice.put(i+1,currentSlice);
+                    currentSlice = negativeSlice.getOrDefault(i + 1, new ArrayList<>());
+                    currentSlice.add(j + 1);
+                    negativeSlice.put(i + 1, currentSlice);
                 }
             }
         }
