915497faea34bb5e502e8d0ad862bcef3108668e
lDFlscreamer <999screamer999@gmail.com>
2020-02-12 20:54:16 +0200

    first step to Spring

---
+++ src/main/java/org/kpi/TheoryOfDecision/Application.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c)  2.2020
+ * This file (Application) is part of BinaryRelationPropertyAnalyser.
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Screamer  <999screamer999@gmail.com>
+ */
+
+package org.kpi.TheoryOfDecision;
+
+public class Application {
+}
